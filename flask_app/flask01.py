3dc3d472ea23c15b16bcd20b96f4632cacea4d5c
ThomasMorgan <tmorga48@uncc.edu>
2021-03-25 13:06:02 -0400

    second version of flask #1

--- flask_app/flask01.py
+++ flask_app/flask01.py
@@ -10,6 +10,7 @@ app = Flask(__name__)     # create an app
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
+@app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
