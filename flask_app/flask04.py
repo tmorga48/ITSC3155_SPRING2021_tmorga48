8a7704afab6cda27a2bdeb7dba6b8d35fbb4a5e8
ThomasMorgan <tmorga48@uncc.edu>
2021-03-30 15:39:45 -0400

    Tester

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -7,6 +7,8 @@ from flask import render_template
 from flask import request
 from flask import redirect, url_for
 from database import db 
+from models import Note as Note
+from models import User as User
 
 app = Flask(__name__)     # create an app
 
@@ -19,7 +21,7 @@ db.init_app(app)
 # Setup models
 with app.app_context():
     db.create_all()   # run under the app context
-    
+
 notes = {1: {'title': 'First Note', 'text': 'This is my first note', 'date': '10-1-2020'},
              2: {'title': 'Second Note', 'text': 'This is my second note', 'date': '10-2-2020'},
              3: {'title': 'Third Note', 'text': 'This is my third note', 'date': '10-3-2020'}
@@ -30,26 +32,28 @@ notes = {1: {'title': 'First Note', 'text': 'This is my first note', 'date': '10
 # get called. What it returns is what is shown as the web page
 @app.route('/index')
 def index():
-    a_user = {'name':'Thomas Morgan', 'email':'mogli@uncc.edu'}
 
+    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
     return render_template('index.html', user = a_user)
 
 @app.route('/notes')
 def get_notes():
-    a_user = {'name':'Thomas Morgan', 'email':'mogli@uncc.edu'}
 
-    return render_template('notes.html', notes = notes, user = a_user)
+    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
+    my_notes = db.session.query(Note).all()
+    return render_template('notes.html', notes = my_notes, user = a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = {'name':'Thomas Morgan', 'email':'mogli@uncc.edu'} 
 
-    return render_template('note.html', note = notes[int(note_id)], user = a_user)
+    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
+    my_note = db.session.query(Note).filter_by(id=note_id)
+    return render_template('note.html', note = my_note, user = a_user)
 
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
-    a_user = {'name':'Thomas Morgan', 'email':'mogli@uncc.edu'}
 
+    a_user = {'name':'Thomas Morgan', 'email':'mogli@uncc.edu'}
     print('request method is', request.method)
     if request.method == 'POST':
 
@@ -60,12 +64,13 @@ def new_note():
         from datetime import date
         today = date.today()
         today = today.strftime("%m-%d-%Y")
+        new_record = Note(title, text, today)
+        db.session.add(new_record)
+        db.session.commit()
 
-        id = len(notes)+1
-        notes[id] = {'title': title, 'text': text, 'date': today}
-
-        return redirect(url_for('get_notes', user = a_user))
+        return redirect(url_for('get_notes'))
     else:
+        a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
         return render_template('new.html', user = a_user)
 
 
