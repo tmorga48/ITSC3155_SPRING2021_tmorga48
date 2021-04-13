7718b31b40b2213791e8581e0f6d5d3aae37abe9
ThomasMorgan <tmorga48@uncc.edu>
2021-04-13 14:37:30 -0400

    adding and displaying comments functionality

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -10,7 +10,8 @@ from flask import session
 from database import db 
 from models import Note as Note
 from models import User as User
-from forms import RegisterForm
+from models import Comment as Comment
+from forms import RegisterForm, LoginForm, CommentForm
 import bcrypt
 from forms import LoginForm
 
@@ -57,9 +58,16 @@ def get_notes():
 @app.route('/notes/<note_id>')
 def get_note(note_id):
 
-    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
-    return render_template('note.html', note = my_note, user = a_user)
+    if session.get('user'):
+        # retrieve note from database
+        my_note = db.session.query(Note).filter_by(id=note_id, user_id=session['user_id']).one()
+
+        # create a comment form object
+        form = CommentForm()
+
+        return render_template('note.html', note=my_note, user=session['user'], form=form)
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
@@ -76,7 +84,7 @@ def new_note():
             today = date.today()
             # format date
             today = today.strftime("%m-%d-%Y")
-            new_record = Note(title, text, today)
+            new_record = Note(title, text, today, session['user_id'])
             db.session.add(new_record)
             db.session.commit()
 
@@ -191,6 +199,23 @@ def logout():
         session.clear()
 
     return redirect(url_for('index'))
+
+@app.route('/notes/<note_id>/comment', methods=['POST'])
+def new_comment(note_id):
+    if session.get('user'):
+        comment_form = CommentForm()
+        # validate_on_submit only validates using POST
+        if comment_form.validate_on_submit():
+            # get comment data
+            comment_text = request.form['comment']
+            new_record = Comment(comment_text, int(note_id), session['user_id'])
+            db.session.add(new_record)
+            db.session.commit()
+
+        return redirect(url_for('get_note', note_id=note_id))
+
+    else:
+        return redirect(url_for('login'))
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)    
